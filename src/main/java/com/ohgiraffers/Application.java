e37dab4c6389aab99b78adf6b3529c9241e4035f
myeonghwan cho <1jmhcho@gmail.com>
2023-05-31 15:41:27 +0900

    hell world 추가

--- src/main/java/com/ohgiraffers/Application.java
+++ src/main/java/com/ohgiraffers/Application.java
@@ -3,6 +3,8 @@ package com.ohgiraffers;
 public class Application {
 
     public static void main(String[] args) {
+
         System.out.println("hello world!!!");
+        System.out.println("hell world!!");
     }
 }
